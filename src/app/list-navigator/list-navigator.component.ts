fc7649114f7b39987051cfd99248c051bbe312a1
Abdallahsaleh1 <abdsaleh537@gmail.com>
2021-06-09 23:38:30 +0300

    V 0.2

--- src/app/list-navigator/list-navigator.component.ts
+++ src/app/list-navigator/list-navigator.component.ts
@@ -7,95 +7,22 @@ import { Component, OnInit } from '@angular/core';
 })
 
 export class ListNavigatorComponent implements OnInit {
-  page1 =[{firstname: "Mike" ,lastName: "alex",age: 22,email:"mike@gmail.com"},{firstname: "jo" ,lastName: "sand",age: 22,email:"jo@gmail.com"}];
-  page2=[{firstname: "sameer" ,lastName: "malik",age: 22,email:"sameer@gmail.com"},{firstname: "sara" ,lastName: "malik",age: 43,email:"sara@gmail.com"}];
-  page3= [{firstname: "jo" ,lastName: "sand",age: 22,email:"jo@gmail.com"}];
-  page4=[{firstname: "abd" ,lastName: "saleh",age: 22,email:"abd@gmail.com"}];
-  page5=[{firstname: "sameer" ,lastName: "malik",age: 22,email:"sameer@gmail.com"}];
-  showpage1 = true;
-  showpage2 = false;
-  showpage3 = false;
-  showpage4 = false;
-  showpage5 = false;
-   count = 1;
-   pageNumber=1;
+  pages =[{firstname: "Mike" ,lastName: "alex",age: 22,email:"mike@gmail.com",page :1},{firstname: "jo" ,lastName: "sand",age: 22,email:"jo@gmail.com",page :1},{firstname: "sameer" ,lastName: "malik",age: 22,email:"sameer@gmail.com",page :1},{firstname: "sara" ,lastName: "malik",age: 43,email:"sara@gmail.com",page :2},{firstname: "jo" ,lastName: "sand",age: 22,email:"jo@gmail.com",page :3},{firstname: "abd" ,lastName: "saleh",age: 22,email:"abd@gmail.com",page :4},{firstname: "sameer" ,lastName: "malik",age: 22,email:"sameer@gmail.com",page :5}];
+ 
+  count = 1;
+  pageNumber=1;
   constructor() { }
 
   ngOnInit(): void {
   }
   showListNumbers(pageNumber: number){
-    if (pageNumber == 1) {
-      this.count = pageNumber
-      this.showpage1 = true;
-      this.showpage2 = false;
-      this.showpage3 = false;
-      this.showpage4 = false;
-      this.showpage5 = false;
-    } else if (pageNumber == 2) {
-      this.count = pageNumber
-      this.showpage1 = false;
-      this.showpage2 = true;
-      this.showpage3 = false;
-      this.showpage4 = false;
-      this.showpage5 = false;
-    } else if (pageNumber == 3) {
-      this.count = pageNumber
-      this.showpage1 = false;
-      this.showpage2 = false;
-      this.showpage3 = true;
-      this.showpage4 = false;
-      this.showpage5 = false;
-    } else if (pageNumber == 4) {
-      this.count = pageNumber
-      this.showpage1 = false;
-      this.showpage2 = false;
-      this.showpage3 = false;
-      this.showpage4 = true;
-      this.showpage5 = false;
-    } else if (pageNumber == 5) {
-      this.count = pageNumber
-      this.showpage1 = false;
-      this.showpage2 = false;
-      this.showpage3 = false;
-      this.showpage4 = false;
-      this.showpage5 = true;
-    }
+    this.count = pageNumber;
   }
 
   showListArrows(number: number){
     
     this.count = this.count + number;
-    if (this.count == 1) {
-      this.showpage1 = true;
-      this.showpage2 = false;
-      this.showpage3 = false;
-      this.showpage4 = false;
-      this.showpage5 = false;
-    } else if (this.count == 2) {
-      this.showpage1 = false;
-      this.showpage2 = true;
-      this.showpage3 = false;
-      this.showpage4 = false;
-      this.showpage5 = false;
-    } else if (this.count == 3) {
-      this.showpage1 = false;
-      this.showpage2 = false;
-      this.showpage3 = true;
-      this.showpage4 = false;
-      this.showpage5 = false;
-    } else if (this.count == 4) {
-      this.showpage1 = false;
-      this.showpage2 = false;
-      this.showpage3 = false;
-      this.showpage4 = true;
-      this.showpage5 = false;
-    } else if (this.count == 5) {
-      this.showpage1 = false;
-      this.showpage2 = false;
-      this.showpage3 = false;
-      this.showpage4 = false;
-      this.showpage5 = true;
-    }
+    
   }
 
 
